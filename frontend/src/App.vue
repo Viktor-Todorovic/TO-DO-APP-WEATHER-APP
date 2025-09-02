f919274810f8fff2161fbaabe85b0747209d946d
Viktor-Todorovic <todorovic.viktor10@gmail.com>
2025-09-02 11:51:55 +0200

    inital commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+
+	<HelloWorld msg="You did it!" />
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="text-red-500">caos svima</h1>
+</template>
+
+<style scoped>
+
+</style>
